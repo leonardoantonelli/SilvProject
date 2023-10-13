e593852d3144b7b463268fe7b91d342d2827595d
leonardoantonelli <antonellileonardodesign@gmail.com>
2023-10-13 23:23:49 +0200

    first t

--- javas.js
+++ javas.js
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
-var toggleBtn = document.getElementById('toggle-btn');
-    toggleBtn.onclick = function() {
-        this.classList.toggle('toggle-btn--dark');
-        document.body.classList.toggle('dark-mode');
-        document.getElementById("demo").innerHTML = '<img class="divisorio" src="./assets/img/home/marro.png">';
-
-=======
-var toggleBtn = document.getElementById('toggle-btn');
-    toggleBtn.onclick = function() {
-        this.classList.toggle('toggle-btn--dark');
-        document.body.classList.toggle('dark-mode');
-        document.getElementById("demo").innerHTML = '<img class="divisorio" src="./assets/img/home/marro.png">';
-
->>>>>>> 29b3b51984bad5d2ea44309442dd1ad681b5dcf3
-    };
+var toggleBtn = document.getElementById("toggle-btn");
+toggleBtn.onclick = function () {
+  this.classList.toggle("toggle-btn--dark");
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    document.getElementById("demo").innerHTML =
+      '<img class="divisorio" src="./assets/img/home/marro.png">';
+  } else {
+    document.getElementById("demo").innerHTML =
+      '<img class="divisorio" src="./assets/img/home/63a8a2c7f3ba3771f8ebba51_Untitled design - 2022-12-25T141922.307.webp">';
+  }
+};

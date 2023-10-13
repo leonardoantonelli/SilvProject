a4969a4ecb20603fb806c942322e26664536ddb0
leonardoantonelli <antonellileonardodesign@gmail.com>
2023-10-13 22:00:37 +0200

    first commit

---
+++ javas.js
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+var toggleBtn = document.getElementById('toggle-btn');
+    toggleBtn.onclick = function() {
+        this.classList.toggle('toggle-btn--dark');
+        document.body.classList.toggle('dark-mode');
+        document.getElementById("demo").innerHTML = '<img class="divisorio" src="./assets/img/home/marro.png">';
+
+=======
+var toggleBtn = document.getElementById('toggle-btn');
+    toggleBtn.onclick = function() {
+        this.classList.toggle('toggle-btn--dark');
+        document.body.classList.toggle('dark-mode');
+        document.getElementById("demo").innerHTML = '<img class="divisorio" src="./assets/img/home/marro.png">';
+
+>>>>>>> 29b3b51984bad5d2ea44309442dd1ad681b5dcf3
+    };
